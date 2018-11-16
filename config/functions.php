fb4227bc1536bd8c3b694067c2581c5b85fe0fc4
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-11-16 17:50:56 +0300

    Update functions.php
    Специально выведенные операции

--- config/functions.php
+++ config/functions.php
@@ -2,24 +2,17 @@
 interface InterFunctions
 {
     public function getMenu();
-	public function getSmalCart();
 }
 
 class SpecialFunctions implements InterFunctions
 {
 	public function getMenu(){
 		return _MenuController::getInstance()->getMenu();
-	}
-
-	public function getSmalCart(){
-		return models\SmalCartModel::getInstance()->getCartData();
 	}	
 }
 
 $retro = new SpecialFunctions();
 $retro->getMenu();
-$retro->getSmalCart();
 
 $menu=$retro::getMenu();
-$smal_cart=$retro::getSmalCart();
 $category_list=models\CategoryModel::getInstance()->getCategoryList_UL(0);
