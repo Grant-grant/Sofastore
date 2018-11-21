837e77402e5812ef208048623b1d2ef07ffa77e7
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-11-21 13:05:10 +0300

    new characteristics view

--- views/ProductView.php
+++ views/ProductView.php
@@ -1,5 +1,7 @@
 <?php namespace views;
 $addcart = '/catalog?in-cart-product-id='.$product["id"];
+$rubl = 'руб.';
+$block = 'block';
 ?>
 <h1  class="wrapper fixcart"  style="clear:left;">
 <a href="/catalog"><input type="button" class="my_acc" value="Назад в каталог"></a>
@@ -14,23 +16,35 @@ $addcart = '/catalog?in-cart-product-id='.$product["id"];
 		<div class="descrip-aboutp"><?=$product['desc']?>	</div>
 		</div>
 		<div class="border">
-		<div class="box-mater">
-		<div class="mater">Цвет:<span> <?=$product['color']?></span></div><div class="mater">Размер:<span> <?=$product['size']?></span></div>
-		</div>
-		<div class="box-mater">
-		<div class="mater">Каркас:<span> <?=$product['fasad_karkas']?></span></div><div class="mater">Наполнение:<span> <?=$product['filling']?></span></div>
-		</div>
-		<div class="box-mater">
-		<div class="mater">Механизм:<span> <?=$product['mechanism']?></span></div><div class="mater">Бельевой ящик:<span> <?=$product['drawer']?></span></div><div class="mater">Чехол:<span> <?=$product['removable_cover']?></span></div>
-		</div>
-		<div class="PricePD"><?=$product['price']?> руб. <span> <s> <?=$product['old_price']?></s> руб.</span></div>
+		<table class="tablesecta">
+		<tr class="box-mater"><td class="mater">Цвет:<span> <?=$product['color']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Размер:<span> <?=$product['size']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Каркас:<span> <?=$product['fasad_karkas']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Наполнение:<span> <?=$product['filling']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Механизм:<span> <?=$product['mechanism']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Бельевой ящик:<span> <?=$product['drawer']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Чехол:<span> <?=$product['removable_cover']?></span></td></tr>		
+		</table>
+		<?php if ($product['old_price']==0.00) {
+			$product['old_price']='' ;
+			$rubl = '';
+		}
+		if (($product['sleep_size']=='')&&($product['complekt']=='')&&($product['color_legs']=='')&&($product['set_size']=='')) {
+			$block = 'none';
+		}
+		?>
+		<div class="PricePD"><?=$product['price']?> руб. <span> <s> <?=$product['old_price']?></s> <?php echo $rubl;?></span></div>
 		<div><a href="<?=$addcart?>"><div class="bottomcart"><img src="../public/images/cart3.png" alt="cart3" class="cartred">Add to Cart</div></a></div>
 		</div>
-		<div class="border box-mater">
+		<div class="border" style="display:<?=$block?>;">
 		<div class="new-text">
-		<div class="box-mater"><div class="mater">Дополнительные параметры</div></div>		
-		<div class="box-mater"><div class="mater">Спальное место:<span> <?=$product['sleep_size']?></span></div><div class="mater">Состав комплекта:<span> <?=$product['complekt']?></span></div></div>	
-		<div class="box-mater"><div class="mater">Цвет ножек:<span> <?=$product['color_legs']?></span></div><div class="mater">Сидячее место:<span> <?=$product['set_size']?></span></div></div>
+		<div class="mater">Дополнительные параметры</div>
+		<table class="tablesecta">
+		<tr class="box-mater"><td class="mater">Спальное место:<span> <?=$product['sleep_size']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Состав комплекта:<span> <?=$product['complekt']?></span></td></tr>	
+		<tr class="box-mater"><td class="mater">Цвет ножек:<span> <?=$product['color_legs']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Сидячее место:<span> <?=$product['set_size']?></span></td></tr>
+		</table>
 		</div>
 		</div>		
 	</section>	
