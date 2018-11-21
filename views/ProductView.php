833208755f44cf0ac4fe2af6a2628bf909fab662
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-11-21 18:11:19 +0300

    edited

--- views/ProductView.php
+++ views/ProductView.php
@@ -14,37 +14,41 @@ $block = 'block';
 		<div class="border">
 		<div class="name-aboutp"><?=$product['name']?></div>	
 		<div class="descrip-aboutp"><?=$product['desc']?>	</div>
-		</div>
-		<div class="border">
-		<table class="tablesecta">
-		<tr class="box-mater"><td class="mater">Цвет:<span> <?=$product['color']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Размер:<span> <?=$product['size']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Каркас:<span> <?=$product['fasad_karkas']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Наполнение:<span> <?=$product['filling']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Механизм:<span> <?=$product['mechanism']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Бельевой ящик:<span> <?=$product['drawer']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Чехол:<span> <?=$product['removable_cover']?></span></td></tr>		
-		</table>
 		<?php if ($product['old_price']==0.00) {
 			$product['old_price']='' ;
 			$rubl = '';
 		}
-		if (($product['sleep_size']=='')&&($product['complekt']=='')&&($product['color_legs']=='')&&($product['set_size']=='')) {
-			$block = 'none';
-		}
+		if ($product['size']=='') $block0 = 'none';
+		if ($product['fasad_karkas']=='') $block1 = 'none';
+		if ($product['filling']=='') $block2 = 'none';
+		if ($product['mechanism']=='') $block3 = 'none';
+		if ($product['drawer']=='') $block4 = 'none';
+		if ($product['removable_cover']=='') $block5 = 'none';
+		if ($product['sleep_size']=='') $block6 = 'none';
+		if ($product['complekt']=='') $block7 = 'none';
+		if ($product['color_legs']=='') $block8 = 'none';
+		if ($product['set_size']=='') $block9 = 'none';			
 		?>
 		<div class="PricePD"><?=$product['price']?> руб. <span> <s> <?=$product['old_price']?></s> <?php echo $rubl;?></span></div>
 		<div><a href="<?=$addcart?>"><div class="bottomcart"><img src="../public/images/cart3.png" alt="cart3" class="cartred">Add to Cart</div></a></div>
-		</div>
-		<div class="border" style="display:<?=$block?>;">
-		<div class="new-text">
-		<div class="mater">Дополнительные параметры</div>
+		</div>		
+		<div class="border">
 		<table class="tablesecta">
-		<tr class="box-mater"><td class="mater">Спальное место:<span> <?=$product['sleep_size']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Состав комплекта:<span> <?=$product['complekt']?></span></td></tr>	
-		<tr class="box-mater"><td class="mater">Цвет ножек:<span> <?=$product['color_legs']?></span></td></tr>
-		<tr class="box-mater"><td class="mater">Сидячее место:<span> <?=$product['set_size']?></span></td></tr>
+		<tr class="box-mater"><td class="mater">Цвет:<span> <?=$product['color']?></span></td></tr>
+		<tr style="display:<?=$block0?>" class="box-mater"><td class="mater">Размер:<span> <?=$product['size']?></span></td></tr>
+		<tr style="display:<?=$block1?>" class="box-mater"><td class="mater">Каркас:<span> <?=$product['fasad_karkas']?></span></td></tr>
+		<tr style="display:<?=$block2?>" class="box-mater"><td class="mater">Наполнение:<span> <?=$product['filling']?></span></td></tr>
+		<tr style="display:<?=$block3?>" class="box-mater"><td class="mater">Механизм:<span> <?=$product['mechanism']?></span></td></tr>
+		<tr style="display:<?=$block4?>" class="box-mater"><td class="mater">Бельевой ящик:<span> <?=$product['drawer']?></span></td></tr>
+		<tr style="display:<?=$block5?>" class="box-mater"><td class="mater">Чехол:<span> <?=$product['removable_cover']?></span></td></tr>		
+		</table>		
+		<table class="tablesecta" >
+		<tr style="display:<?=$block6?>" class="box-mater"><td class="mater">Спальное место:<span> <?=$product['sleep_size']?></span></td></tr>
+		<tr style="display:<?=$block7?>" class="box-mater"><td class="mater">Состав комплекта:<span> <?=$product['complekt']?></span></td></tr>	
+		<tr style="display:<?=$block8?>" class="box-mater"><td class="mater">Цвет ножек:<span> <?=$product['color_legs']?></span></td></tr>
+		<tr style="display:<?=$block9?>" class="box-mater"><td class="mater">Сидячее место:<span> <?=$product['set_size']?></span></td></tr>
 		</table>
-		</div>
+		
+		
 		</div>		
 	</section>	
