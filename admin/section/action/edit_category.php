a559b0bee9e66615902501f60a24078dea9d0dd7
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-01 22:42:41 +0300

    Delete edit_category.php

--- admin/section/action/edit_category.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id=$_POST['id'];
-
-if(models\CategoryModel::getInstance()->editCategory($id,$_POST)) 
-$response=array("msg"=>"Не удалось изменить категорию!", "status"=>false); 
- else $response=array("msg"=>"Изменена категория!", "status"=>true);
-echo json_encode($response);
