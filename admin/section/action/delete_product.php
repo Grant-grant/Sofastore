250f3fa80267dd43c7dac10fb3f2c80825d12873
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-01 22:42:30 +0300

    Delete delete_product.php

--- admin/section/action/delete_product.php
+++
@@ -1,7 +0,0 @@
-<?php
-$model=new models\ProductModel;	
-if($model->deleteProduct($_POST['id']))
-	$response=array("msg"=>"Не удалось удалить товар!","status"=>false);
-else
-	$response=array("msg"=>"Удален товар","status"=>true);	
-echo json_encode($response);
