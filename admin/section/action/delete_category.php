adde5dc6fdb4e5145b54e932d1182128b2d7d7be
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-01 22:42:47 +0300

    Delete delete_category.php

--- admin/section/action/delete_category.php
+++
@@ -1,6 +0,0 @@
-<?php
-if($id=models\CategoryModel::getInstance()->delCategory($_POST['id']))
-	$response=array("msg"=>"Не удалось удалить категорию!","status"=>false);
-else
-	$response=array("msg"=>"Удалена категория","status"=>true);	
-echo json_encode($response);
