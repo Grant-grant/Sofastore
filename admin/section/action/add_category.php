9648dbec2c31a0a89e2072630147c0130f7c3d95
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-01 22:42:18 +0300

    Delete add_category.php

--- admin/section/action/add_category.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($id=models\CategoryModel::getInstance()->addCategory($_POST)){
-	$response=array("msg"=>"Не удалось создать категорию!","status"=>false);
-} else {
-$response=array("msg"=>"Создана категория","status"=>true);
-}	
-echo json_encode($response);
