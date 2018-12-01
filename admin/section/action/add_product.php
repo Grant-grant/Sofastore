10aed855b26a275399e2af2a6bcd4e208b07409e
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-01 22:42:25 +0300

    Delete add_product.php

--- admin/section/action/add_product.php
+++
@@ -1,7 +0,0 @@
-<?php
-$model=new models\ProductModel;	
-if($id=$model->addProduct($_POST))
-	$response=array("msg"=>"не удалось создать товар!","status"=>false);
-else
-	$response=array("msg"=>"Создан товар","status"=>true);	
-echo json_encode($response);
