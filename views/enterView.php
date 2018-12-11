26267c02ae986f7faaad9a8a3dbbe8ee0e076c9c
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-12 02:09:53 +0300

    Update enterView.php

--- views/enterView.php
+++ views/enterView.php
@@ -10,9 +10,9 @@ echo $msg;
 
 <form action="/enter" method="POST">
 <div align="center" ><div align="center" >
-<br>
-<b>login:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" size="15" maxlength="40" name="login" value="<?=$login?>" /><br />
-<b>Пароль:</b><input type="text" size="15" name="pass" value="<?=$pass?>" /><br /> </div> 
+<br><div class="cat">Укажите логин и пароль для авторизации</div><br />
+<b><label class="label-w">login:</label></b><input type="text" size="15" maxlength="40" name="login" value="<?=$login?>" /><br />
+<b><label class="label-w">Пароль:</label></b><input type="password" size="15" name="pass" value="<?=$pass?>" /><br /> </div> 
  <div align="center" > <br><br><input class="my_acc2 btsearch" value="Авторизоваться" id="btn_login" type="submit"><br><a href="/reg"><br />  
   <input class="my_acc2 btsearch" value="Регистрация" type="button"></a></div> 
 </form>
