2787f76fe12ce13b50ed18d25136e70c86e31b24
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-12 02:17:25 +0300

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -6,26 +6,53 @@
 				
 				<div class="row">
 				<div class="col-md-3">
-				<div class="cat">Нижнее меню</div><br>
+				<div class="cat-top ">Нижнее меню</div><br>
 				<div class="Objective"><a href="/Sofa">ДИВАНЫ И КРЕСЛА</a><br><a href="/Closet">ШКАФЫ</a><br><a href="/Bedroom">СПАЛЬНЯ</a><br>
 				<a href="/Hall">ПРИХОЖААЯ</a><br><a href="/Kitchen">КУХНЯ</a><br><a href="/Children">ДЕТСКАЯ</a><br><a href="/Office">КАБИНЕТ</a><br><br></div>
 				</div>
 				<div class="col-md-4">
-				<div class="cat">Сертификаты</div>
+				<div class="cat-top ">Сертификаты</div>
 				<ul>
-				<div class="Objective"><img src="../public/images/sertif.png"></div>
+				<div class="Objective"><style>.blokimg {position: relative;	}
+				.overlay{
+				    display: none;
+				    height: auto;
+				    left: -15%;
+				    position: absolute;
+				    top: -85%;
+				    width: auto;
+				    z-index: 999;}
+				    .overlay .overlay_container{
+				        display: table-cell;
+				        vertical-align: middle;}
+				        .overlay_container img{
+				            background-color: #AB5;
+				            padding: 1px;
+				            -webkit-border-radius: 5px;
+				            -moz-border-radius: 5px;}
+				            .overlay:target {
+				                display: table;}
+				                </style>
+            	<div class="blokimg">
+            	<div class="overlay" id="contenedor1">
+            	<div class="overlay_container">
+            	<a href="#close"><img src="../public/images/big-sertif.png"/></a></div>
+            	</div>
+            	<a href="#contenedor1">	<img src="../public/images/sertif.png" id="imagenM1" /></a>
+            	</div></div>
 				</ul>
 				</div>	
 				<div class="col-md-5">
-				<div class="cat">Как к нам проехать</div>
+				<div class="cat-top ">Как к нам проехать</div>
 				<ul>
 				<div class="Objective"><script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A493d9b6c864c7ec283667581cd887ef3d1af7e65f1630a74f185e32759f3be06&amp;width=467&amp;height=284&amp;lang=ru_RU&amp;scroll=true"></script></div>
 				</ul>
 				</div>	
 				</div>	
-				</div></div></div>
-
-				<div class="footers"><div class="copiriteflex"><div class="wrapper">
+				</div></div>
+				
+				
+				<div class="copiriteflex"><div class="wrapper">
 				<div class="networks">
 					© <?php echo date("Y");?>  THE GRAND<span> SOFA</span>  All Rights Reserved.
 				</div>	
@@ -36,5 +63,8 @@
 					<a href="https://ru.pinterest.com/" target="_blank"><div class="awesometilda"><i class="fa cor fa-pinterest-p" aria-hidden="true"></i></div></a>
 					<a href="https://plus.google.com/discover" target="_blank"><div class="awesometilda"><i class="fa fa-google-plus" aria-hidden="true"></i></div></a>
 				</div>
-				</div></div></div>
+				</div></div>
+				
+				</div>
+			
 			</div>	
