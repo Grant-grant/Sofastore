ee3438e75933e1214ae7240a4f6ec4144c8b2436
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-12-12 02:16:53 +0300

    Update header.php

--- template/header.php
+++ template/header.php
@@ -1,26 +1,25 @@
 		<div id="header" class="wrapper">
 			</br>		
 			<div class="logo">
-				
 			</div>		
-		<section class="box2">
-			<a href="/enter"><div class="my_acc" style="float:right;">Authentication<i class="fa fa-caret-down awesamepb" aria-hidden="true"></i></div></a>
+          <section class="box2">
+			<a href="/enter"><div class="my_aut" style="float:right;">Authentication</div></a>
 			<div class="smalcart">				
 				<br/>				
 				<?if  ($smal_cart['cart_count']>0) {?>	
-				<z class="goodchek"><?=$smal_cart['cart_count']?> </z><b> Сумма:</b>  <b style="color: #4fc35d;"><?=$smal_cart['cart_price']?> руб.</b></z>
-				<a class="cart_buy"  href='/cart'>checkout</a>
+				<a class=""  href='/cart'><p class="goodchek"><?=$smal_cart['cart_count']?> </p></a><b> К Оплате:</b><br><b class="PricePD-header"><?=$smal_cart['cart_price']?> руб.</b>
+				
 				<? }?>	
 			</div>	
-		</section>		
+		</section>				
 		<section class="box2">
-		<div class="wrapper slideshow box-slide">
-		<div class="textbrand-slide">THE GRAND<span> SOFA</span><br> <h2 class="line2">FURNITURE<span> STORE</span></h2></div>
-		<div class="textbrand-slide">  <a href="skype:+79999999999?call">Skype</a></div>
+		<div class="slideshow box-slide">
+		<div class="textbrand-slide"><span>G</span>RAND<span> S</span>OFA<br> <h2 class="line2">FURNITURE<span> STORE</span></h2></div>
+		<div class="skype_cl">  <a href="skype:+79999999999?call"><img src="../public/images/Scype.png"></a></div>
 		</div>
 		</section>
 			
 		<div class="menu-width"><?=$menu?></div>		
 			
 		</div>	
-		<div  id="content">
+		<div  class="wrapper" id="content">
